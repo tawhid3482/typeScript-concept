7e68bb0ca410c990463078f9baac077958a91af0
saikat <tawhidulislam3482@gmail.com>
2024-04-30 23:45:12 +0600

    learning funtion

--- module1/src/1-6.ts
+++ module1/src/1-6.ts
@@ -1,4 +1,16 @@
 // learning function
-function add(num1,num2){
-return add()
+function add(num1:number,num2:number):number{
+return num1 + num2
+}
+add(451,75)
+
+const sub =(num1:number,num2:number):number=>num1+num2
+
+// object -- function -- method
+const poorUser = {
+    UserName:'Saikat',
+    balance : 25,
+    addBalance(balance:number){
+        return this.balance + balance
+    }
 }
