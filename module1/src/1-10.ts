2c4636a61d1a2876ccd7e42807c8867dfa4f4d6c
saikat <tawhidulislam3482@gmail.com>
2024-05-01 11:21:24 +0600

    interSection tpye

--- module1/src/1-10.ts
+++ module1/src/1-10.ts
@@ -1,6 +1,27 @@
 {
     // union type
 
-    type FrontEnd = 'ami'| 'tumi'
-    const frontEnd:FrontEnd = 'ami'
+    type FrontEnd = 'ami' | 'tumi'
+    type FullStack = 'expert' | 'junior'
+    type dev = FrontEnd | FullStack
+    const frontEnd: FrontEnd = 'ami'
+
+
+    type Front = {
+        skills: string[];
+        designation1: 'front end'
+    }
+
+    type backend = {
+        skills: string[];
+        designation2: 'back end'
+    }
+    type fullStack = Front & backend
+
+    const dev: fullStack = {
+        skills: ['html', 'css'],
+        designation1:'front end',
+        designation2: 'back end'
+    }
+
 }
