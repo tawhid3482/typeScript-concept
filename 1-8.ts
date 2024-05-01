52ecea3e7cd67c1b946fe2d8c37ca4b22056c76b
saikat <tawhidulislam3482@gmail.com>
2024-05-01 09:43:13 +0600

    array and object destructure learning

--- 1-8.ts
+++ 1-8.ts
@@ -1,13 +1,21 @@
 {
     // learning destructure
     const user = {
-        name:'saikat',
-        hoby:{
-             game:'pubg',
-             sport:'football'
+        name: 'saikat',
+        hoby: {
+            game: 'pubg',
+            sport: 'football'
         },
         contact: '0186316'
     }
-    
-    const {contact,hoby:{game}}=user
+
+    const { contact, hoby: { game } } = user
+
+
+    // array destructuring
+    const bestFriend = ['ami', 'tumi', 'apni']
+    const [, , bestBondo, ...rest] = bestFriend
+
+
+
 }
