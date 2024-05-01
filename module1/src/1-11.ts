df13576952437e9ed20bcfba9ca94f77d85c0ea5
saikat <tawhidulislam3482@gmail.com>
2024-05-01 16:21:42 +0600

    nullish operator

--- module1/src/1-11.ts
+++ module1/src/1-11.ts
@@ -1,9 +1,19 @@
 {
     // learn about ??
-    const age:number = 15
+    const age:number = 17
     if(age >=18 ){
         console.log('adult')
     }else{
-        console.log('child')
+        console.log('childs')
     }
+
+    const isAdult = age >=18 ? 'adult':'child'
+    console.log({isAdult})
+
+    //nullish operator
+    // only for null || undefined use case
+    const isAuthenticated = null
+    const result1 = isAuthenticated ?? 'Guest'
+    console.log({result1})
+
 }
