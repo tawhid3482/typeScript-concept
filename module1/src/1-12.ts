2532052cb891366db117f13255b2f2f9f520ee69
saikat <tawhidulislam3482@gmail.com>
2024-05-01 16:35:39 +0600

    nullish operator

---
+++ module1/src/1-12.ts
@@ -0,0 +1,11 @@
+{
+    //nullable
+    const search = (value:string)=>{
+        if(value){
+            console.log('searching')
+        }else{
+            console.log('no search')
+        }
+    }
+    search('ami')
+}
