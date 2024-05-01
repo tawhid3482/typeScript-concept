eba458e5ed27590335391cc814e1f7c342a00f0f
saikat <tawhidulislam3482@gmail.com>
2024-05-01 16:53:36 +0600

    unknown and never type

--- module1/src/1-12.ts
+++ module1/src/1-12.ts
@@ -1,11 +1,36 @@
 {
     //nullable
-    const search = (value:string)=>{
+    const search = (value:string | null)=>{
         if(value){
             console.log('searching')
         }else{
             console.log('no search')
         }
     }
-    search('ami')
+    search(null)
+
+    // unknown type
+    const getSpeedInMiterPerSecond = (value:unknown)=>{
+        if(typeof value === 'number'){
+            const convertedSpeed =( value *1000)/3600
+            console.log(`The Speed is ${convertedSpeed} ms^-1`)
+        }
+        else if (typeof value === 'string'){
+            const [result,unit] = value.split(' ',1)
+            const convertedSpeed =( parseFloat(result) *1000)/3600
+            console.log(`The Speed is ${convertedSpeed} ms^-1`)
+           
+        }
+        else{
+            console.log('wrong input')
+        }
+    }
+    getSpeedInMiterPerSecond(null)
+
+
+    // never type
+    const  throwError=(msg:string):never=>{
+        throw new Error (msg)
+    }
+    throwError('error hoye gase')
 }
