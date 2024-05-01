8536b9d2a4b9420d3e02867aa159aa350b88c3f8
saikat <tawhidulislam3482@gmail.com>
2024-05-01 10:01:38 +0600

    type alias

---
+++ module1/src/1-9.ts
@@ -0,0 +1,21 @@
+{
+    // type alias 
+    const student1 : {
+        name:string;
+        age:number;
+        numberPH:string
+    }={
+        name:'saikat',
+        age:4,
+        numberPH:'16594621'
+    }
+    const student2 : {
+        name:string;
+        age:number;
+        numberPH:string
+    }={
+        name:'tamim',
+        age:44,
+        numberPH:'165954621'
+    }
+}
