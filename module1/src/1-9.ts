b4e9f90b7e4d88f80e31b68f8d6cb8a991413a2b
saikat <tawhidulislam3482@gmail.com>
2024-05-01 10:08:24 +0600

    type alias of function

--- module1/src/1-9.ts
+++ module1/src/1-9.ts
@@ -1,21 +1,29 @@
 {
     // type alias 
-    const student1 : {
+    type Student = {
         name:string;
         age:number;
         numberPH:string
-    }={
+    }
+    const student1 : Student={
         name:'saikat',
         age:4,
         numberPH:'16594621'
     }
-    const student2 : {
-        name:string;
-        age:number;
-        numberPH:string
-    }={
+    const student2 : Student={
         name:'tamim',
         age:44,
         numberPH:'165954621'
     }
+
+
+type UserName = string
+const userName: UserName = 'sa'
+type booleanValue = boolean
+const isAdmin:booleanValue = true 
+
+
+type Add=(num1:string, num2:string)=>string
+const add :Add = (num1,num2)=>num1+num2
+
 }
