59ae71a8547af32438d8a2f123d922b089737263
saikat <tawhidulislam3482@gmail.com>
2024-05-01 16:31:16 +0600

    nullish operator

--- module1/src/1-11.ts
+++ module1/src/1-11.ts
@@ -1,19 +1,42 @@
 {
     // learn about ??
-    const age:number = 17
-    if(age >=18 ){
+    const age: number = 17
+    if (age >= 18) {
         console.log('adult')
-    }else{
+    } else {
         console.log('childs')
     }
 
-    const isAdult = age >=18 ? 'adult':'child'
-    console.log({isAdult})
+    const isAdult = age >= 18 ? 'adult' : 'child'
+    console.log({ isAdult })
 
     //nullish operator
     // only for null || undefined use case
-    const isAuthenticated = null
+    const isAuthenticated = ''
     const result1 = isAuthenticated ?? 'Guest'
-    console.log({result1})
+    const result2 = isAuthenticated ? isAuthenticated : 'Guest'
+    console.log({ result1 }, { result2 })
+
+    type User = {
+        name: string;
+        address: {
+            city: string;
+            phone: string;
+            srNo: number;
+            road?:string
+
+        }
+    }
+    const user :User  ={
+        name:'saikat',
+        address:{
+            city:'Madhupur',
+            phone:'56555',
+            srNo:45,
+            
+        }
+    }
+    const roadNo = user?.address?.road ?? 'NO road Number'
+    console.log({roadNo})
 
 }
