14c0592ce0c00ada2034b1ca94b68a6c23c2c258
saikat <tawhidulislam3482@gmail.com>
2024-05-02 19:08:20 +0600

    type assertion complete

--- module2/2-1.ts
+++ module2/2-1.ts
@@ -5,4 +5,27 @@
     anything = 'hi ';
     anything = 55;
     // (anything as number).
+
+    const kgTogm =(value:string | number):string | number |undefined=>{
+        if(typeof value === 'string'){
+            const convertedValue = parseFloat(value) *1000
+            return convertedValue
+        }
+        else if(typeof value === "number"){
+            return value *1000
+        }
+    }
+    const result1 = kgTogm(45) as number
+    const result2 = kgTogm('54') as string
+
+
+    type customError = {
+        message: string
+    }
+
+    try{
+
+    }catch(error){
+        console.log((error as customError).message)
+    }
 }
