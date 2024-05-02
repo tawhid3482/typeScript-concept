e00c0b5e912ba8c2cbcb6b75c12114841ae40f85
saikat <tawhidulislam3482@gmail.com>
2024-05-02 21:18:52 +0600

    generic dynamic  type

--- module2/2-4.ts
+++ module2/2-4.ts
@@ -1,13 +1,48 @@
 {
     // 
-    interface Dev<T> {
+    interface Dev<T,X=null> {
         name:string;
         info:{
             skills:string;
             title:string;
-        }
-        smartWatch: T
+        };
+        smartWatch: T;
+        bike? : X
     } 
+    type juniorWatch = {
+        name2:string;
+        model:string
+    }
 
-    
+    const juniorDev :Dev<juniorWatch> = {
+        name:'saikat',
+        info:{
+            skills:'html css',
+            title:'junior'
+        },
+        smartWatch:{
+            name2:'apple',
+            model:'dw65'
+        }
+    }
+
+    interface loveWatch {name2:string,model:string,price:number}
+    interface loveBike {model:string}
+    const richDev :Dev<loveWatch,loveBike > = {
+        name:'tawhid',
+        info:{
+            skills:'html css javascript',
+            title:'senior'
+        },
+        smartWatch:{
+            name2:'sunStar',
+            model:'dw65',
+            price:464,
+
+        },
+        bike:{
+            model:'ami'
+        }
+    }
+
 }
