2d8daa4b7d64ad9dddfea70bcd8429e95b554621
saikat <tawhidulislam3482@gmail.com>
2024-05-02 19:14:19 +0600

    interface type

---
+++ module2/2-2.ts
@@ -0,0 +1,17 @@
+{
+    // interface learning
+    // object ar jnno type alias o interface 2 ty use kora jabo
+
+    type user1 = {
+        name:string;
+        age:number
+    }
+    interface user2 {
+        name:string;
+        age:number
+    }
+    const user : user2={
+        name:'tawhid',
+        age:45
+    }
+}
