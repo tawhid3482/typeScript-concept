3cca42f76e62b64ca74ebaa19211194dc13f9747
saikat <tawhidulislam3482@gmail.com>
2024-05-03 15:08:34 +0600

    utility Record  omit readOnly Required

--- module2/2-11.ts
+++ module2/2-11.ts
@@ -11,7 +11,30 @@
     //Omit
     type NameAge = Omit<person ,'name'|'age'>
     //required
-    type NameRequired = Required<person>
+    type NameRequired = Required<person> 
     //partial
     type NamePartial = Partial<person>
+
+    // readOnly
+    type readOnlyPerson = Readonly <person> 
+
+    const person1 :readOnlyPerson = {
+        name:'saia',
+        age:55
+    }
+    // person1.name='ami'
+
+    // Record type 
+    // type myObj = {
+    //     name:string;
+    //     age:number;
+    // }
+    type myObj = Record<string,string>
+    const emtryObj :Record<string,unknown>= {}
+    const obj:myObj = {
+        name:'aik',
+        age:'54',
+        c:'cdc'
+    }
+
 }
