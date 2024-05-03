fb707ea54c76a75395a4ecac0519f99884f81083
saikat <tawhidulislam3482@gmail.com>
2024-05-03 10:13:30 +0600

    conditional type

--- module2/2-9.ts
+++ module2/2-9.ts
@@ -1,8 +1,16 @@
 {
-// conditional type
-type a1 = null
-type b = undefined
-type A = a1 extends null ? true : false // conditional type
-type B= a1 extends null ? true : b extends undefined ? undefined : any // conditional type
+    // conditional type
+    type a1 = null
+    type b = undefined
+    type A = a1 extends null ? true : false // conditional type
+    type B = a1 extends null ? true : b extends undefined ? undefined : any // conditional type
 
+
+    type vehicle = {
+        bike: string;
+        car: string;
+        ship: string
+    }
+    type checkVehicle<T> = T extends 'bike' | 'car' | 'ship' ? true : false
+    type result = checkVehicle<'bike'>
 }
