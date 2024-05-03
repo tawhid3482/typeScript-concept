3895bc39620f04cd3a6846cd3e10ba4fba52bb84
saikat <tawhidulislam3482@gmail.com>
2024-05-03 12:17:18 +0600

    mapped type

--- module2/2-10.ts
+++ module2/2-10.ts
@@ -3,4 +3,21 @@
     const arrofNumber : number[]= [2,66,3]
  const arrOfstring: string[]= arrofNumber.map((number)=>number.toString())
  console.log(arrOfstring)
+
+type areaNum = {
+    height:number;
+    width:number
+}
+// type arrString = {
+//     height:string;
+//     width:string
+// }
+type height = areaNum['height'] // look up type
+type areaString<T> = {
+  [key in keyof T] :T[key]
+}
+const res1 :areaString<{height:string, width:number}>={
+    height:'45',
+    width:5
+}
 }
