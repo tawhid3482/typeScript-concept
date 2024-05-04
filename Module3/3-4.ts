703f0717418d045567ac06cf172ab5b7d9ac9222
saikat <tawhidulislam3482@gmail.com>
2024-05-04 23:15:34 +0600

    oop type guard

--- Module3/3-4.ts
+++ Module3/3-4.ts
@@ -3,32 +3,49 @@
     class Animal {
         //    public name:string;
         //    public  sound:string;
-            constructor(public name:string,public sound:string){
-                // this.name = name,
-                // this.sound = sound
-            }
-            makeSound(){
-                console.log(`THIS ${this.name} say ${this.sound}`)
-            }
+        constructor(public name: string, public sound: string) {
+            // this.name = name,
+            // this.sound = sound
         }
-        class dog extends Animal {
-            constructor(name:string, sound:string){
-                super(name,sound)
-            }
-            makeBar(){
-                console.log(`Dog barking`)
-            }
+        makeSound() {
+            console.log(`THIS ${this.name} say ${this.sound}`)
         }
-        class cat extends Animal {
-            constructor(name:string, sound:string){
-                super(name,sound)
-            }
-            makeMaw(){
-                console.log(`cat maw`)
-            }
+    }
+    class dog extends Animal {
+        constructor(name: string, sound: string) {
+            super(name, sound)
         }
+        makeBar() {
+            console.log(`Dog barking`)
+        }
+    }
+    class cat extends Animal {
+        constructor(name: string, sound: string) {
+            super(name, sound)
+        }
+        makeMaw() {
+            console.log(`cat maw`)
+        }
+    }
 
-        const Dog = new dog ('dog dog','by')
-        const Cat = new cat ('cat cat','asi')
-        Do
+     const isDog =(animal : Animal):animal is dog=>{
+        return animal instanceof dog
+     }
+     const isCat =(animal : Animal):animal is cat=>{
+        return animal instanceof cat
+     }
+
+    const getAminal = (animal: Animal) => {
+        if (isDog(animal)) {
+
+            animal.makeBar()
+        }else if(isCat(animal)){
+            animal.makeMaw()
+        }else{
+            animal.makeSound()
+        }
+    }
+    const Dog = new dog('dog dog', 'by')
+    const Cat = new cat('cat cat', 'asi')
+    getAminal(Cat)
 }
