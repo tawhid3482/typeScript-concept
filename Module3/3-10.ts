c99167758ab6e5ee4c816f27d56b9caba1b0ea0e
saikat <tawhidulislam3482@gmail.com>
2024-05-05 11:09:00 +0600

    secret

--- Module3/3-10.ts
+++ Module3/3-10.ts
@@ -5,7 +5,7 @@
     class BankAccount {
         public readonly id:number;
         public name:string;
-        private _balance : number;
+        protected _balance : number;
         constructor(  id: number,   name: string,
              _balance: number){
                 this.id= id
@@ -16,19 +16,20 @@
             addDeposit(amount : number ){
                 this._balance = this._balance + amount    
             }
-            getBalance (){
+           private getBalance (){
                 return this._balance
             }
            
-            class StudentAccount extends BankAccount{
-                test(){
-                    this.
-                }
-            }
+            // class StudentAccount extends BankAccount{
+            //     test(){
+            //         this._balance
+            //     }
+            // }
     }
 
     const goribAcc = new BankAccount(11,'ami',898)
-    goribAcc.addDeposit(4545)
+    // goribAcc
     const mybalance = goribAcc.getBalance()
     console.log(mybalance)
+    m
 }
